22c914a7f4da433f775c7277ee9ef6a276634b45
pizi26 <piziwawa@gmail.com>
2018-12-26 15:39:57 +0900

    comething new

--- Alexeev_Tarabukin/AddPatients.php
+++ Alexeev_Tarabukin/AddPatients.php
@@ -4,15 +4,20 @@ $link = mysqli_connect($server, $user, $password, $database)
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
 
 
-$First_name = mysqli_real_escape_string($link, $_POST['First name']);
-$Last_name = mysqli_real_escape_string($link, $_POST['Last name']);
+$FirstName = mysqli_real_escape_string($link, $_POST['First Name']);
+$LastName = mysqli_real_escape_string($link, $_POST['Last Name']);
+$FathersName = mysqli_real_escape_string($link, $_POST['Fathers Name']);
+$PassportSeries = mysqli_real_escape_string($link, $_POST['Passport Series']);
+$PassportNumber = mysqli_real_escape_string($link, $_POST['Passport Number']);
+$Adress = mysqli_real_escape_string($link, $_POST['Adress']);
 echo $FirstName;
 echo $LastName;
+echo $FathersName;
 echo $PassportSeries;
 echo $PassportNumber;
 echo $Adress
 
-$SQLquery = "INSERT INTO Patients VALUES ((SELECT IFNULL(max(id)+1,1) from (Select id from Patients) as idPatients), '$FirstName','$LastName', '$PassportSeries', '$PassportNumber', '$Adress')";
+$SQLquery = "INSERT INTO Patients VALUES ((SELECT IFNULL(max(id)+1,1) from (Select id from Patients) as idPatients), '$FirstName','$LastName', '$FathersName','$PassportSeries', '$PassportNumber', '$Adress')";
 echo '<BR> SQL query: ';
 echo $SQLquery;
 
