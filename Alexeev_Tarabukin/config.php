c1e33b051348fe0ca070e2fec3d690540e4c7e20
pizi26 <piziwawa@gmail.com>
2018-12-27 12:16:04 +0900

    comething new

--- Alexeev_Tarabukin/config.php
+++ Alexeev_Tarabukin/config.php
@@ -1,6 +1,6 @@
 <?php
-$server = '10.14.129.132';
-$user='AlekseevStanislav';
-$password='sPCErRWy';
+$server = '52.14.19.253';
+$user='Stas';
+$password='mozzik98';
 $database='AlekseevStanislavDB';
 ?>
