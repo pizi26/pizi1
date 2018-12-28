4b4132c91a02def0afd6286fb5346180b4657f77
pizi26 <piziwawa@gmail.com>
2018-12-29 05:58:22 +0900

    comething new

--- Alexeev_Tarabukin/AddSessions.php
+++ Alexeev_Tarabukin/AddSessions.php
@@ -3,18 +3,17 @@ include('config.php');
 $link = mysqli_connect($server, $user, $password, $database)
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
 
-$Passport = mysqli_real_escape_string($link, $_POST['Passport']);
+$specId = mysqli_real_escape_string($link, $_POST['specId']);
 $timeStart = mysqli_real_escape_string($link, $_POST['timeStart']);
 $timeFinish = mysqli_real_escape_string($link, $_POST['timeFinish']);
-
-$FathersName = mysqli_real_escape_string($link, $_POST['FathersName']);
-
+$pathId = mysqli_real_escape_string($link, $_POST['pathId']);
 $iddiagnosis = mysqli_real_escape_string($link, $_POST['iddiagnosis']);
 $lechenie = mysqli_real_escape_string($link, $_POST['lechenie']);
 $sledSpec = mysqli_real_escape_string($link, $_POST['sledSpec']);
 
 
-$SQLquery = "INSERT INTO sessions VALUES ((SELECT IFNULL(max(idsessions)+1,1) from (Select idsessions from sessions) as idsessions),'$Passport', '$FirstName','$LastName', '$FathersName','$ScienceDegree')";
+$SQLquery = "INSERT INTO sessions VALUES ((SELECT IFNULL(max(idsessions)+1,1) from (Select idsessions from sessions) as idsessions),'$specId', '$timeStart','$timeFinish', '$pathId','$iddiagnosis','$lechenie','$sledSpec')";
+$SQLquery1 = "INSERT INTO PatientsDiagnosis VALUES ('$pathId','$iddiagnosis')";
 
 if (mysqli_query($link, $SQLquery)) {
     echo "<BR>Можно реализовать потом какой нибудь простой виджетик, вывод: Работник добавлен";
