798f5ca2e3e91488fa5f3bd3302b9fce12656481
pizi26 <piziwawa@gmail.com>
2018-12-29 04:07:30 +0900

    comething new

--- Alexeev_Tarabukin/PatientsDiagnosis.php
+++ Alexeev_Tarabukin/PatientsDiagnosis.php
@@ -5,7 +5,8 @@
  </head>
  <body>
 	<?php
-	printf('<P>Связь пациент-диагноз(чтобы потом использовать в карте пациента):</P>');
+	printf('<P>Связь пациент-диагноз(чтобы потом использовать в карте пациента),
+		в идеале этой таблицы не будет на сайте, только в базе данных, но в данном этапе разработки решили выставить на показ все таблицы дб, тут инфа не вводится а автоматически сохранятся после сеанса</P>');
 	include('config.php');	
 	$link = mysqli_connect($server, $user, $password, $database)
 	    or die('Error: Unable to connect: ' . mysqli_connect_error());
@@ -15,7 +16,7 @@
 	$SQLresult = mysqli_query($link,$SQLquery);
 	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 	{
-		printf('<P>пациент %d -  диагноз %d </P>',$result[0],$result[1]);
+		printf('<P>У пациента %d -  диагноз %d </P>',$result[0],$result[1]);
 	}
 	mysqli_free_result($SQLresult);
 	mysqli_close($link);
