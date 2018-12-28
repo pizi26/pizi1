2d45beee35ec5fec9d56506976a8b12ec267d20d
pizi26 <piziwawa@gmail.com>
2018-12-29 06:56:00 +0900

    comething new

--- Alexeev_Tarabukin/AddSessions.php
+++ Alexeev_Tarabukin/AddSessions.php
@@ -12,7 +12,9 @@ $lechenie = mysqli_real_escape_string($link, $_POST['lechenie']);
 $sledSpec = mysqli_real_escape_string($link, $_POST['sledSpec']);
 
 
-$SQLquery = "INSERT INTO sessions VALUES ((SELECT IFNULL(max(idsessions)+1,1) from (Select idsessions from sessions) as idsessions),'$specId', '$timeStart','$timeFinish', '$pathId','$iddiagnosis','$lechenie','$sledSpec'), INSERT INTO PatientsDiagnosis VALUES ('$pathId','$iddiagnosis')";
+$SQLquery = "INSERT INTO sessions VALUES ((SELECT IFNULL(max(idsessions)+1,1) from (Select idsessions from sessions) as idsessions),'$specId', '$timeStart','$timeFinish', '$pathId','$iddiagnosis','$lechenie','$sledSpec')";
+
+/* INSERT INTO PatientsDiagnosis VALUES ('$pathId','$iddiagnosis')" - не понял как добавить в две разные таблиц, SQLquery тока с последним insert работает*/
 
 if (mysqli_query($link, $SQLquery)) {
     echo "<BR>Можно реализовать потом какой нибудь простой виджетик, вывод: Работник добавлен";
