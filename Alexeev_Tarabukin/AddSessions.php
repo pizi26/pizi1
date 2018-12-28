09f3d1b3424138e730d9b2d15b6a93f80ae6d4b4
pizi26 <piziwawa@gmail.com>
2018-12-29 07:07:01 +0900

    comething new

--- Alexeev_Tarabukin/AddSessions.php
+++ Alexeev_Tarabukin/AddSessions.php
@@ -4,9 +4,9 @@ $link = mysqli_connect($server, $user, $password, $database)
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
 
 $specId = mysqli_real_escape_string($link, $_POST['specId']);
+$pathId = mysqli_real_escape_string($link, $_POST['pathId']);
 $timeStart = mysqli_real_escape_string($link, $_POST['timeStart']);
 $timeFinish = mysqli_real_escape_string($link, $_POST['timeFinish']);
-$pathId = mysqli_real_escape_string($link, $_POST['pathId']);
 $iddiagnosis = mysqli_real_escape_string($link, $_POST['iddiagnosis']);
 $lechenie = mysqli_real_escape_string($link, $_POST['lechenie']);
 $sledSpec = mysqli_real_escape_string($link, $_POST['sledSpec']);
@@ -17,7 +17,7 @@ $SQLquery = "INSERT INTO sessions VALUES ((SELECT IFNULL(max(idsessions)+1,1) fr
 /* INSERT INTO PatientsDiagnosis VALUES ('$pathId','$iddiagnosis')" - не понял как добавить в две разные таблиц, SQLquery тока с последним insert работает*/
 
 if (mysqli_query($link, $SQLquery)) {
-    echo "<BR>Можно реализовать потом какой нибудь простой виджетик, вывод: Работник добавлен";
+    echo "<BR>Можно реализовать потом какой нибудь простой виджетик, вывод: cеанс добавлен";
 } else {
     echo "<BR>Error: " . $sql . "<br>" . mysqli_error($link);
 }
